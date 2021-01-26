8f282be266729e934a4c15f2ecc044651af4490c
Hyunmin Kim <landiajuve@gmail.com>
2021-01-26 17:53:20 +0900

    - 로그인 체크 함수

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,21 @@
 (function($) {
  
-	console.log($);
+	var $idVal = $('#input-id').val();
+	var $pwVal = $('#input-pw').val();
+	var $logInBtn = $('.login-button');
+
+	function loginCheck() {
+		$logInBtn.click(function(){
+			if(($idVal = "test") && ($pwVal = 1234)){
+				location.href = 'list.html';
+			} else {
+				alert('id/pw가 맞지 않습니다.');
+			}
+		});
+	}
+	
+	loginCheck();
+
+
 
 })(jQuery);
