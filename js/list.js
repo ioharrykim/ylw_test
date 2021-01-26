1105c8966cecb6b242a2ecb7ec511a844a0f7158
Hyunmin Kim <landiajuve@gmail.com>
2021-01-26 15:24:34 +0900

    - list.html 각 강의 썸네일 클릭시 팝업창 제작 (영상 임베딩 완료, 2019 Live Surgery 영상 1개는 아직 영상 수정중)

--- js/list.js
+++ js/list.js
@@ -3,9 +3,10 @@ console.log("Script Load");
     $(document).ready(function() {
         console.log('jQuery Ready');
         
+        /* 설문조사 팝업 */
         var $pBtn = $('.poll-button');
         var $dim = $('.dim');
-        var $popUp = $('.popup');
+        var $popUp = $('.poll');
         var $cBtn = $('.close-button');
 
         $pBtn.click(function(){
@@ -18,7 +19,110 @@ console.log("Script Load");
             console.log("닫기 버튼");
             $dim.hide();
             $popUp.hide();
+            $lec1.hide();
+            $lec2.hide();
+            $lec3.hide();
+            $lec4.hide();
+            $lec5.hide();
+            $lec6.hide();
+            $lec7.hide();
+            $lec8.hide();
+            $lec9.hide();
+            $lec10.hide();
+            $lec11.hide();
             $('html').css("overflow-y","auto");
+        });
+
+        /* 영상 팝업 */
+        var $thumb1 = $('.thumb-1'),
+            $thumb2 = $('.thumb-2'),
+            $thumb3 = $('.thumb-3'),
+            $thumb4 = $('.thumb-4'),
+            $thumb5 = $('.thumb-5'),
+            $thumb6 = $('.thumb-6'),
+            $thumb7 = $('.thumb-7'),
+            $thumb8 = $('.thumb-8'),
+            $thumb9 = $('.thumb-9'),
+            $live1 = $('.live-surgery1'),
+            $live2 = $('.live-surgery2-1'),
+            $live3 = $('.live-surgery2-2');
+        var $lec1 = $('.lecture1'),
+            $lec2 = $('.lecture2'),
+            $lec3 = $('.lecture3'),
+            $lec4 = $('.lecture4'),
+            $lec5 = $('.lecture5'),
+            $lec6 = $('.lecture6'),
+            $lec7 = $('.lecture7'),
+            $lec8 = $('.lecture8'),
+            $lec9 = $('.lecture9'),
+            $lec10 = $('.lecture10'),
+            $lec11 = $('.lecture11');
+        
+        $thumb1.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec1.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $thumb2.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec2.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $thumb3.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec3.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $thumb4.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec4.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $thumb5.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec5.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $thumb6.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec6.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $thumb7.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec7.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $thumb8.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec8.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $live1.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec9.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $live2.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec10.show();
+            $('html').css("overflow-y","hidden");
+        });
+        $live3.click(function(){
+            console.log("클릭");
+            $dim.show();
+            $lec11.show();
+            $('html').css("overflow-y","hidden");
         });
 
         
